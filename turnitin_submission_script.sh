95dbd58efda2129f81ce3fc7b5399439c3906a12
shubham shukla <shubhamshuklaerssss@gmail.com>
2015-04-01 18:14:41 +0530

    Changed the way to specify files to include now you can pass type of files to ignore just like gitignore

--- turnitin_submission_script.sh
+++ turnitin_submission_script.sh
@@ -1,36 +1,62 @@
 #!/bin/bash
 
-#This script combines all files in a given folder with given extension into one file
-#and then can also recreate the same dir structure from the same file
+#This script combines all files in a given folder with given extension into one combined_file
+#and then can also recreate the same dir structure from the same combined_file
 #useful for submitting projects on turnitin
 #NO GUARANTEE
 
 working_dir="."
-extension_list=""
-file="file.txt"
+filter_file_name=""
+combined_file="combined_file.txt"
 action="combine"
+filter_string=""
+
+function parse_filter_file(){
+    filter_string=""
+    temp=""
+    if [ -n "$filter_file_name" ]
+    then
+        #cat $filter_file_name | while read line will not work if we want the val of variable which was updated inside loop
+        #cause the piplining creates while in a subshell with own set of variables which are destroyed when while is finished
+        while read -r line
+        do
+            if [ -n "$filter_string" ]
+            then
+                filter_string="$filter_string -or"
+            fi
+            filter_string="$filter_string -path \"$working_dir/$line\""
+        done < "$filter_file_name"
+
+        if [ -n "$filter_string" ]
+        then
+            filter_string=" ! \\( $filter_string \\) "
+        fi
+    fi
+}
 
 
 function combine(){
-    echo "" > $file
+    echo "" > $combined_file
 
-    for entry in $(find "$working_dir")
+    #find dir will recursively list all files in the system in grep -v is used to invert selection
+    find_command="find $working_dir $filter_string"
+    #eval is used to execute a string
+    find_result=$(eval "$find_command")
+
+    for entry in $find_result
     do
         if [ -f "$entry" ]
         then
-            if [ "$(stat -L -c "%d:%i" $entry)" != "$(stat -L -c "%d:%i" $file)" ] && [ "$(stat -L -c "%d:%i" $entry)" != "$(stat -L -c "%d:%i" $0)" ] && [ "$(stat -L -c "%d:%i" $entry)" != "$(stat -L -c "%d:%i" $working_dir)" ]
+            #same file will have same divise_id and inode number... -L is used to follow link
+            if [ "$(stat -L -c "%d:%i" $entry)" != "$(stat -L -c "%d:%i" $combined_file)" ] && [ "$(stat -L -c "%d:%i" $entry)" != "$(stat -L -c "%d:%i" $0)" ]
             then
-                file_ext=${entry##*.}
-                if [[ $extension_list =~ $file_ext ]]
-                then
-
-                    echo "%%%%%%$entry%%%%%%" >> $file
-                    echo "">>$file
-                    cat $entry >>$file
-                    echo "">>$file
-                    echo "">>$file
-                    echo "">>$file
-                fi
+                echo "$entry"
+                echo "%%%%%%$entry%%%%%%" >> $combined_file
+                echo "">>$combined_file
+                cat $entry >>$combined_file
+                echo "">>$combined_file
+                echo "">>$combined_file
+                echo "">>$combined_file
             fi
         fi
     done
@@ -39,7 +65,7 @@ function combine(){
 
 function split(){
     output_file=""
-    cat $file | while read line
+    cat $combined_file | while read line
     do
         if [[ $line =~ ^%%%%%%(.*)%%%%%%$ ]]
         then
@@ -57,35 +83,42 @@ function split(){
 }
 
 
-while getopts "d:e:f:a:" opt; do
+while getopts "d:m:f:a:" opt; do
     case $opt in
         d)
             working_dir=$OPTARG
             ;;
-        e)
-            extension_list=$OPTARG
+        m)
+            filter_file_name=$OPTARG
             ;;
         f)
-            file=$OPTARG
+            combined_file=$OPTARG
             ;;
         a)
             action=$OPTARG
             ;;
         \?)
             echo "Invalid syntax"
-            echo "Usage ./turnitin_submission_script.sh [-d dir] [-e extension_list] [-f file] [-a action]"
+            echo "Usage ./turnitin_submission_script.sh [-d dir] [-m filter_file_name] [-f combined_file] [-a action]"
             echo "extension list is a string with space seperated desired extensions with quote"
             echo "eg\"txt cpp\""
             echo "here only txt and cpp files will be combined"
             echo "action is either spilt or combine"
-            echo "with action split arg of -d will act as output dir and arg of -f will act as input file"
-            echo "with action combine arg of -d will act as input dir and arg of -f will act as output file"
+            echo "with action split arg of -d will act as output dir and arg of -f will act as input combined_file"
+            echo "with action combine arg of -d will act as input dir and arg of -f will act as output combined_file"
+            echo "you pass a filter file with -m option it act similar to gitignore file"
+            echo "the format of filter_file is you give one ignore exprression on each line you can use shell wildcards basically similar to .gitignore"
+            echo "though unlike .gitignore this can only be used to ignore files you cannot use negation"
             ;;
     esac
 done
 
+#remove the suffix / if present
+working_dir=${working_dir%\/}
+
 if [ "$action" == "combine" ]
 then
+    parse_filter_file
     combine
 elif [ "$action" == "split" ]
 then
