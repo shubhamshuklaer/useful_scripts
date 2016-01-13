a4139fdb8266745d525a8b539772dfa030431836
Shubham Shukla <shubhamshuklaerssss@gmail.com>
2016-01-13 19:15:34 +0000

    faster download and also functionality to download part of video

--- youtube_downloader.bash
+++ youtube_downloader.bash
@@ -1,16 +1,75 @@
-#!/bin/bash
+#!/bin/bash -i
 #there is one optional arg which will be passed to -f in youtube-dl
+
+# in getopts the first : disables verbose error reporting
+# After than if colon comes after the option it means that it requires an
+# argument
+
+format="5"
+download_part=false
+
+function usage {
+    echo "$0 [-f format] [-s start_time -t duration]"
+}
+
+while getopts ":f:s:t:" opt; do
+  case $opt in
+    f)
+        format=$OPTARG
+        ;;
+    s)
+        download_part=true
+        start_time=$OPTARG
+        ;;
+    t)
+        download_part=true
+        duration=$OPTARG
+        ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 cd $(xdg-user-dir DOWNLOAD)
-if [[ $# -eq 1 ]]
-then
-    format=$1
-else
-    format="5"
-fi
+
 url=$(xclip -o)
-notify-send "Downloading" "Url: $url \n Format: $format"
-eval "youtube-dl -f $format $url"
-if [[ $? -eq 0 ]]
+body="Url: $url \n Format: $format "
+# -n will check for variable not empty
+if [[ -n "$start_time" ]]
+then
+    body="$body \n Start time: $start_time"
+fi
+if [[ -n "$duration" ]]
+then
+    body="$body \n Duration time: $duration"
+fi
+
+notify-send "Downloading" "$body"
+down_url=`youtube-dl -f $format -g $url`
+file_name=`youtube-dl -f $format --get-filename $url`
+if $download_part
+then
+    extension="${file_name##*.}"
+    base_file_name="${file_name%.*}"
+    # http://superuser.com/questions/377343/cut-part-from-video-file-from-start-position-to-end-position-with-ffmpeg
+    # -ss must be used before -i for seek see man ffmpeg
+    ffmpeg -ss "$start_time" -i "$down_url" -t "$duration" -c copy "$base_file_name""_start=$start_time""_duration=$duration"".$extension"
+    ret_val=$?
+else
+    # pd is a bash function wrapping pycurl-download
+    pd "$file_name" "$down_url"
+    # TODO the ret_val causes problem with pd as its not the ret val of
+    # download but the du -h loop. Fix this
+    ret_val=$?
+fi
+
+if [[ $ret_val -eq 0 ]]
 then
     notify-send "Download Status" "Success"
 else
